01c3377a444cdcf3fa36101b6f06ab9cdb133ef4
Mikhail1995-25 <mikhail_pushkarev_1995@mail.ru>
2020-02-07 19:58:12 +0300

    AlertDivByZero[#230934]

--- src/main/java/ru/job4j1/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j1/condition/AlertDivByZero.java
@@ -1,4 +1,17 @@
 package ru.job4j1.condition;
 
 public class AlertDivByZero {
+    public static void main (String[]args){
+       possibleDiv(0);
+       possibleDiv(-4);
+
+    }
+    public static void possibleDiv(int number){
+        if(number == 0){
+            System.out.println("Could not div by 0");
+        }else{
+            System.out.println("This is negative numbers");
+        }
+    }
+
 }
