1e57f12d4344da861db27579b83c43570df7dc58
Mikhail1995-25 <mikhail_pushkarev_1995@mail.ru>
2020-02-09 14:36:25 +0300

    Переназначение переменной.[#232354]

---
+++ chapter_002/src/main/java/ru/job4j/calculator/Calculators3.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Calculators3 {
+    public static void main(String[]args){
+        int size = 10;
+        System.out.println(size);
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+    }
+}
